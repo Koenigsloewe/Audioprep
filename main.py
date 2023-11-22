134c340f5688010eac2da9f551eca8dc66aa46ec
Koenigsloewe <ThithuSiva@protonmail.com>
2023-11-22 11:00:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QApplication
 from app.gui import AudioprepGUI
 
+
 def main():
     app = QApplication(sys.argv)
     app.setFont(QFont("Roboto", 12))
@@ -11,22 +12,9 @@ def main():
     window.show()
     sys.exit(app.exec_())
 
+
 if __name__ == "__main__":
     main()
 
-
-"""
-    Audioprep/
-|-- app/
-|   |-- __init__.py
-|   |-- convert.py
-|   |-- analyze.py
-|   |-- segment.py
-|   |-- gui.py
-|-- main.py
-|-- requirements.txt
-
-    :return:
-"""
 
 
